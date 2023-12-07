a90b6f7d2fd013488c51af6ed90ae497f5f8f751
Rory Pearson <52637194+Mezmerizxd@users.noreply.github.com>
2023-12-07 16:23:12 +0000

    Add custom folder support for saving screenshots

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        string baseFolder = "maxshot1382";
+        string customFolder = null;
+
 #if DEBUG
         int screenWidth = 2560;
         int screenHeight = 1440;
@@ -15,10 +18,11 @@ class Program
         string imageName = "test_1";
         int offsetX = 0;
         int offsetY = 0;
+        customFolder = "test";
 #else
         if (args.Length < 7)
         {
-            Console.WriteLine("Usage: maxshot1382.exe <screenWidth> <screenHeight> <imageWidth> <imageHeight> <imageName> <offsetX> <offsetY>");
+            Console.WriteLine("Usage: maxshot1382.exe <screenWidth> <screenHeight> <imageWidth> <imageHeight> <imageName> <offsetX> <offsetY> [customFolder]");
             return;
         }
 
@@ -40,16 +44,27 @@ class Program
             Console.WriteLine("Invalid arguments. Please provide integer values for all arguments.");
             return;
         }
+
+        if (args.Length > 7)
+        {
+            customFolder = args[7];
+        }
+        if (customFolder == null)
+        {
+            Console.WriteLine("Invalid arguments. Please provide integer values for all arguments.");
+            return;
+        }
 #endif
 
+        string savePath = GetSavePath(baseFolder, customFolder, imageName);
 
         // Capture a screenshot of the specified region
         Bitmap screenshot = CaptureScreenCenter(screenWidth, screenHeight, imageWidth, imageHeight, offsetX, offsetY);
 
         // Save the screenshot as a PNG image
-        SaveAsPng(screenshot, imageName + ".webp");
+        SaveAsPng(screenshot, savePath);
 
-        Console.WriteLine("Screenshot saved as " + imageName + ".webp");
+        Console.WriteLine("Screenshot saved as " + savePath);
     }
 
     static Bitmap CaptureScreenCenter(int screenWidth, int screenHeight, int imageWidth, int imageHeight, int offsetX, int offsetY)
@@ -79,5 +94,20 @@ class Program
     static void SaveAsPng(Bitmap bitmap, string outputPath)
     {
         bitmap.Save(outputPath, ImageFormat.Webp);
+    }
+
+    static string GetSavePath(string baseFolder, string customFolder, string imageName)
+    {
+        string folderPath = baseFolder;
+
+        if (!string.IsNullOrEmpty(customFolder))
+        {
+            folderPath = Path.Combine(baseFolder, customFolder);
+        }
+
+        // Create directory if it doesn't exist
+        Directory.CreateDirectory(folderPath);
+
+        return Path.Combine(folderPath, imageName + ".webp");
     }
 }
